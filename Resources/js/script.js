75dc61e3942db663c654f28919f1f06a8c12eee3
krunal-lia <krunallia18@gmail.com>
2018-06-15 12:10:28 +0530

    Intital commit

---
+++ Resources/js/script.js
@@ -0,0 +1,12 @@
+
+$('.btn-1').click(function()
+    {
+        $('.login').hide(1);
+        $('.sign-up').show(1);
+   });
+
+$('.btn-2').click(function()
+    {
+        $('.sign-up').hide(1);
+        $('.login').show(1);
+   });
